07daa0438ee9d6ee87bb8952b5fb53734c5fb211
Nikolay Pomogaev <klagefurt@gmail.com>
2024-12-12 20:30:04 +0300

    Added multiconverter

---
+++ WpfApp/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object p, CultureInfo c);
+
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("ConvertBAck method is not implemented");
+        }
+    }
+}
---
+++ WpfApp/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert(object[] values, Type targetType, object p, CultureInfo c)
+        {
+            var collection = new CompositeCollection();
+            foreach (var item in values) 
+            {
+                collection.Add(item);
+            }
+            return collection;
+        }
+    }
+}
